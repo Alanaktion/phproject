6f8197d7c4a7b5987b0142de26a6c290335969eb
Alan Hardman <alanaktion@gmail.com>
2013-12-31 11:42:46 -0700

    Adding basic administration panel

--- app/controller/admin.php
+++ app/controller/admin.php
@@ -5,9 +5,11 @@ namespace Controller;
 class Admin extends Base {
 
 	public function index($f3, $params) {
-
 		echo \Template::instance()->render("admin/index.html");
+	}
 
+	public function users($f3, $params) {
+		echo \Template::instance()->render("admin/users.html");
 	}
 
 }
