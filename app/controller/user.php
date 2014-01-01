8d8a1a8ae9ec0dd990a179aed71d1243e54b14c7
Alan Hardman <alanaktion@gmail.com>
2013-12-31 17:10:44 -0700

    Adding some basic account stuff

--- app/controller/user.php
+++ app/controller/user.php
@@ -13,7 +13,20 @@ class User extends Base {
 	}
 
 	public function account($f3, $params) {
-		$f3->error(404);
+		echo \Template::instance()->render("user/account.html");
+	}
+
+	public function single($f3, $params) {
+		$user = new \Model\User;
+		$user->load(array("username = ?", $params["username"]));
+
+		if($user->id) {
+			$f3->set("this_user", $user->cast());
+			echo \Template::instance()->render("user/single.html");
+		} else {
+			$f3->error(404);
+		}
+
 	}
 
 }
