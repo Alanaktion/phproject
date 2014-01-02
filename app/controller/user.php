6ca8fe6ad535fcca1fe45e3d67cdd32340c11aea
Alan Hardman <alanaktion@gmail.com>
2014-01-02 11:24:11 -0700

    Add account saving and global error/success message handling

--- app/controller/user.php
+++ app/controller/user.php
@@ -19,6 +19,57 @@ class User extends Base {
 		echo \Template::instance()->render("user/account.html");
 	}
 
+	public function save($f3, $params) {
+		$id = $this->_requireLogin();
+
+		$f3 = \Base::instance();
+		$post = array_map("trim", $f3->get("POST"));
+
+		$user = new \Model\User();
+		$user->load(array("id = ?", $id));
+
+		if(!empty($post["old_pass"])) {
+
+			$security = \Helper\Security::instance();
+
+			// Update password
+			if($security->bcrypt_verify($user->password, $post["old_pass"])) {
+				if(strlen($post["new_pass"]) >= 6) {
+					$user->password = $security->bcrypt($post["new_pass"]);
+					$f3->set("success", "Password updated successfully.");
+				} else {
+					$f3->set("error", "New password must be at least 6 characters.");
+				}
+			} else {
+				$f3->set("error", "Current password entered is not valid.");
+			}
+
+		} else {
+
+			// Update profile
+			if(!empty($post["name"])) {
+				$user->name = filter_var($post["name"], FILTER_SANITIZE_STRING);
+			} else {
+				$error = "Please enter a name.";
+			}
+			if(filter_var($post["email"], FILTER_VALIDATE_EMAIL)) {
+				$user->email = $post["email"];
+			} else {
+				$error = "Please enter a valid email address.";
+			}
+
+			if(empty($error)) {
+				$f3->set("success", "Profile updated successfully.");
+			} else {
+				$f3->set("error", $error);
+			}
+
+		}
+
+		$user->save();
+		echo \Template::instance()->render("user/account.html");
+	}
+
 	public function single($f3, $params) {
 		$this->_requireLogin();
 
@@ -31,7 +82,6 @@ class User extends Base {
 		} else {
 			$f3->error(404);
 		}
-
 	}
 
 }
