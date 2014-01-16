ffaf26278793a997a5a306b2131bcfc91063f6e9
Alan Hardman <alanaktion@gmail.com>
2014-01-16 10:40:08 -0700

    Fixing breadcrumb order when project ID is greater than task ID

--- app/model/issue.php
+++ app/model/issue.php
@@ -10,16 +10,18 @@ class Issue extends Base {
 		$f3 = \Base::instance();
 		$db = $f3->get("db.instance");
 		return $db->exec(
-"SELECT _id AS id, name FROM (
-	SELECT @r AS _id, name, (
+"SELECT _id AS id, `name` FROM (
+	SELECT @r AS _id, `name`, @lvl := @lvl + 1 AS lvl, (
 		SELECT @r := parent_id FROM issue n
 		WHERE id = _id
 	) FROM (
-		SELECT @r := '{$this->id}'
+		SELECT @r := '{$this->id}',
+		@lvl := 0
 	) vars, issue c
 ) c
 WHERE _id > 1
-GROUP BY _id", null, 60);
+GROUP BY _id
+ORDER BY lvl DESC", null, 60);
 	}
 
 	public static function clean($string) {
