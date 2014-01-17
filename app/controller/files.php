235d78f67c81953558fe95b4e4b8fdcf785dd9c9
Alan Hardman <alanaktion@gmail.com>
2014-01-17 16:57:14 -0700

    Forgot to add the file... here it is.

---
+++ app/controller/files.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controller;
+
+class Files extends Base {
+
+	public function thumb($f3, $params) {
+		$file = new \Model\Issue\File();
+		$file->load($params["id"]);
+
+		if(!$file->id) {
+			$f3->error(404);
+			return;
+		}
+
+		$img = new \Image($file->disk_filename, null, $f3->get("ROOT") . "/");
+		$img->resize($params["size"], $params["size"]);
+		$img->render("jpeg");
+	}
+
+}
