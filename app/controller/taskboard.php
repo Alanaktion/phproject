9bbb57bb7c92b9c0e2f07de54dad57772783d5cb
Alan Hardman <ahardman@thrivelife.com>
2014-01-21 13:47:37 -0700

    Dynamically built taskboard (heck yeah :D)

--- app/controller/taskboard.php
+++ app/controller/taskboard.php
@@ -5,6 +5,7 @@ namespace Controller;
 class Taskboard extends Base {
 
 	public function index($f3, $params) {
+        $this->_requireLogin();
 
         // Load the requested sprint
         $sprint = new \Model\Sprint();
@@ -14,25 +15,31 @@ class Taskboard extends Base {
             return;
         }
         $f3->set("sprint", $sprint->cast());
+        $f3->set("title", $sprint->name);
 
         // Load issue statuses
-        $status = new \Model\Issue();
+        $status = new \Model\Custom("issue_status");
         $statuses = $status->paginate(0, 20);
         $f3->set("statuses", $statuses);
 
         // Load project list
-        $issue = new \Model\Issue();
+        $issue = new \Model\Custom("issue_user");
         $projects = $issue->paginate(0, 100, array("sprint_id = ? AND deleted_date IS NULL", $sprint->id), array("order" => "owner_id ASC"));
 
         // Build multidimensional array of all tasks and projects
         $taskboard = array();
         foreach($projects["subset"] as $project) {
 
+            // Build array of statuses to put tasks under
+            $columns = array();
+            foreach($statuses["subset"] as $status) {
+                $columns[$status["id"]] = array();
+            }
+
             // Get all tasks under the project, put them under their status
             $tasks = $issue->paginate(0, 100, array("parent_id = ? AND deleted_Date IS NULL", $project["id"]), array("order" => "due_date ASC"));
-            $columns = array();
             foreach($tasks["subset"] as $task) {
-                $columns[$task["status"]] = $task;
+                $columns[$task["status"]][] = $task;
             }
 
             // Add hierarchial structure to taskboard array
@@ -42,14 +49,18 @@ class Taskboard extends Base {
             );
 
         }
-
         $f3->set("taskboard", $taskboard);
+
+        // Get user list for select
+        $users = new \Model\User();
+        $f3->set("users", $users->paginate(0, 1000, "deleted_date IS NULL", array("order" => "name ASC")));
 
 		echo \Template::instance()->render("taskboard/index.html");
 
 	}
 
     public function test($f3) {
+        $this->_requireLogin();
         echo \Template::instance()->render("taskboard/test.html");
     }
 
