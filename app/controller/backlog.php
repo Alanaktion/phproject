28c8cf6f0d47f953ca5f96bf1c942cf74bf95b28
Alan Hardman <ahardman@thrivelife.com>
2014-01-22 08:18:34 -0700

    Require login on /backlog

--- app/controller/backlog.php
+++ app/controller/backlog.php
@@ -5,8 +5,9 @@ namespace Controller;
 class Backlog extends Base {
 
 	public function index($f3, $params) {
+        $this->_requireLogin();
 		echo \Template::instance()->render("backlog/index.html");
 	}
-	
+
 
 }
