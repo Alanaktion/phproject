ee0be5dc6da6cd0127e9b0b5e0a6d9407f66adc4
Alan Hardman <ahardman@thrivelife.com>
2014-04-15 15:13:24 -0700

    Human readable helper for issue updates

---
+++ app/helper/update.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Helper;
+
+class Update extends \Prefab {
+
+	function humanReadableValues($field, $old_val, $new_val) {
+		switch($field) {
+			case "owner_id":
+			case "author_id":
+				// Load old and new user and get names
+				$user = new \Model\User;
+				$user->load($old_val);
+				$old_val = $user->name;
+				$user->load($new_val);
+				$new_val = $user->name;
+				break;
+			case "status":
+				// Do stuff
+				break;
+		}
+		return array($old_val, $new_val);
+	}
+
+}
