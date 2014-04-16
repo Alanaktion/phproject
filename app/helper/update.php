42d29e05e544c1c6aafba6cbcaf19ba9679311ce
Alan Hardman <ahardman@thrivelife.com>
2014-04-16 10:01:01 -0600

    Improving issue history, adding thumbnail cache config

--- app/helper/update.php
+++ app/helper/update.php
@@ -5,7 +5,7 @@ namespace Helper;
 class Update extends \Prefab {
 
 	function humanReadableValues($field, $old_val, $new_val) {
-		switch($field):
+		switch($field) {
 			case "owner_id":
 			case "author_id":
 				// Load old and new user and get names
@@ -17,13 +17,21 @@ class Update extends \Prefab {
 				break;
 			case "status":
 				// Load old and new status and get names
-				$status = new \Model\issue\status;
+				$status = new \Model\Issue\Status;
 				$status->load($old_val);
 				$old_val = $status->name;
 				$status->load($new_val);
 				$new_val = $status->name;
 				break;
-		endswitch;
+			case "priority":
+				// Load old and new priority and get names
+				$priority = new \Model\Issue\Priority;
+				$priority->load(array("value = ?", $old_val));
+				$old_val = $priority->name;
+				$priority->load(array("value = ?", $new_val));
+				$new_val = $priority->name;
+				break;
+		}
 		return array("old" => $old_val, "new" => $new_val);
 	}
 
