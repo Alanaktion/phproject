062097fff6c44ae190bbb6c15705db7257fe80cf
Alan Hardman <ahardman@thrivelife.com>
2014-04-22 09:44:54 -0600

    Update ALL the headers!

--- app/helper/notification.php
+++ app/helper/notification.php
@@ -80,7 +80,7 @@ class Notification extends \Prefab {
 
 				// To send HTML mail, the Content-type header must be set
 				$headers  = 'MIME-Version: 1.0' . "\r\n";
-				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
 				// Additional headers
 				$headers .= 'To: '.$recipient . "\r\n";
@@ -121,7 +121,7 @@ class Notification extends \Prefab {
 
 				// To send HTML mail, the Content-type header must be set
 				$headers  = 'MIME-Version: 1.0' . "\r\n";
-				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
 				// Additional headers
 				$headers .= 'To: '.$recipient . "\r\n";
