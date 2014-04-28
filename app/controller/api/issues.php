08626f57be6a7961e499082dd68f62b4391373a4
Alan Hardman <ahardman@thrivelife.com>
2014-04-28 14:52:50 -0600

    Issue creation in the API

--- app/controller/api/issues.php
+++ app/controller/api/issues.php
@@ -70,6 +70,7 @@ class Issues extends \Controller\Api\Base {
 		return array_replace($casted, $result);
 	}
 
+	// Get a list of issues
 	public function get($f3, $params) {
 		$issue = new \Model\Issue\Detail();
 		$result = $issue->paginate(
@@ -90,10 +91,110 @@ class Issues extends \Controller\Api\Base {
 		));
 	}
 
+	// Create a new issue
 	public function post($f3, $params) {
+		if($_POST) {
+			// By default, use standard HTTP POST fields
+			$post = $_POST;
+		} else {
 
+			// For Redmine compatibility, also accept a JSON object
+			try {
+				$post = json_decode(file_get_contents('php://input'), true);
+			} catch (Exception $e) {
+				print_json(array(
+					"error" => "Unable to parse input"
+				));
+				return false;
+			}
+
+			if(!empty($post["issue"])) {
+				$post = $post["issue"];
+			}
+
+			// Convert Redmine names to Phproject names
+			if(!empty($post["subject"])) {
+				$post["name"] = $post["subject"];
+			}
+			if(!empty($post["parent_issue_id"])) {
+				$post["parent_id"] = $post["parent_issue_id"];
+			}
+			if(!empty($post["tracker_id"])) {
+				$post["type_id"] = $post["tracker_id"];
+			}
+			if(!empty($post["assigned_to_id"])) {
+				$post["owner_id"] = $post["assigned_to_id"];
+			}
+			if(!empty($post["fixed_version_id"])) {
+				$post["sprint_id"] = $post["fixed_version_id"];
+			}
+
+		}
+
+		// Ensure a status ID is added
+		if(!empty($post["status_id"])) {
+			$post["status"] = $post["status_id"];
+		}
+		if(empty($post["status"])) {
+			$post["status"] = 1;
+		}
+
+		// Verify the required "name" field is passed
+		if(empty($post["name"])) {
+			$this->_error("The 'name' field is required.");
+		}
+
+		// Verify given values are valid (types, statueses, priorities)
+		if(!empty($post["type_id"])) {
+			$type = new \Model\Issue\Type;
+			$type->load($post["type_id"]);
+			if(!$type->id) {
+				$this->_error("The 'type_id' field is not valid.");
+			}
+		}
+		if(!empty($post["parent_id"])) {
+			$parent = new \Model\Issue;
+			$parent->load($post["parent_id"]);
+			if(!$parent->id) {
+				$this->_error("The 'type_id' field is not valid.");
+			}
+		}
+		if(!empty($post["status"])) {
+			$status = new \Model\Issue\Status;
+			$status->load($post["status"]);
+			if(!$status->id) {
+				$this->_error("The 'status' field is not valid.");
+			}
+		}
+		if(!empty($post["priority_id"])) {
+			$priority = new \Model\Issue\Priority;
+			$priority->load(array("value" => $post["priority_id"]));
+			if(!$priority->id) {
+				$this->_error("The 'priority_id' field is not valid.");
+			}
+		}
+
+		// Create a new issue based on the data
+		$issue = new \Model\Issue();
+		$issue->author_id = $this->_userId;
+		$issue->name = trim($post["name"]);
+		$issue->type_id = empty($post["type_id"]) ? 1 : $post["type_id"];
+		$issue->priority_id = empty($post["priority_id"]) ? 0 : $post["priority_id"];
+		if(!empty($post["description"])) {
+			$issue->description = $post["description"];
+		}
+		if(!empty($post["parent_id"])) {
+			$issue->parent_id = $post["parent_id"];
+		}
+		if(!empty($post["assigned_to_id"])) {
+			$issue->owner_id = $post["owner_id"];
+		}
+
+		$issue->save();
+		print_json($issue->cast());
 	}
 
+	// Get a single issue's details
 	public function single_get($f3, $params) {
 		$issue = new \Model\Issue\Detail();
 		$issue->load($params["id"]);
@@ -104,12 +205,20 @@ class Issues extends \Controller\Api\Base {
 		}
 	}
 
+	// Update a single issue
 	public function single_put($f3, $params) {
 
 	}
 
+	// Delete a single issue
 	public function single_delete($f3, $params) {
+		$issue = new \Model\Issue;
+		$issue->load($params["id"]);
+		$issue->delete();
 
+		print_json(array(
+			"deleted" => $params["id"]
+		));
 	}
 
 }
