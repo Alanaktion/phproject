48fc6f96e71c45dbc693a24bd22f37040a02e12b
rightbit <ryan@trailjack.com>
2014-05-27 11:41:40 -0600

    notifications to groups

--- app/helper/notification.php
+++ app/helper/notification.php
@@ -77,7 +77,9 @@ class Notification extends \Prefab {
 
 	// Send an email to watchers detailing the updated fields
 	public function issue_create($issue_id) {
+
 		$f3 = \Base::instance();
+		$log = new \Log("mail.log");
 		if($f3->get("mail.from")) {
 			$log = new \Log("mail.log");
 
@@ -107,14 +109,38 @@ class Notification extends \Prefab {
 	// Get array of email addresses of all watchers on an issue
 	protected function _issue_watchers($issue_id) {
 		$f3 = \Base::instance();
+		$log = new \Log("mail.log");
 		$db = $f3->get("db.instance");
 		$recipients = array();
 
 		// Add issue author and owner
 		$result = $db->exec("SELECT u.email FROM issue i INNER JOIN `user` u on i.author_id = u.id WHERE i.id = :id", array("id" => $issue_id));
-		$recipients[] = $result[0]["email"];
+		if(!empty( $result[0]["email"])) {
+			$recipients[] = $result[0]["email"];
+		}
+
+
 		$result = $db->exec("SELECT u.email FROM issue i INNER JOIN `user` u on i.owner_id = u.id WHERE i.id = :id", array("id" => $issue_id));
-		$recipients[] = $result[0]["email"];
+		if(!empty( $result[0]["email"])) {
+			$recipients[] = $result[0]["email"];
+		}
+
+		//add whole group
+
+		$result = $db->exec("SELECT u.role, u.id FROM issue i INNER JOIN `user` u on i.owner_id = u.id  WHERE i.id = :id", array("id" => $issue_id));
+		if($result && $result[0]["role"] == 'group') {
+			$group_users = $db->exec("SELECT g.user_email FROM user_group_user g  WHERE g.group_id = :id", array("id" => $result[0]["id"]));
+			foreach($group_users as $group_user) {
+
+				$log->write($group_user["user_email"]);
+				if(!empty( $group_user["user_email"])) {
+					$recipients[] = $group_user["user_email"];
+				}
+
+			}
+
+		}
+
 
 		// Add watchers
 		$watchers = $db->exec("SELECT u.email FROM issue_watcher w INNER JOIN `user` u ON w.user_id = u.id WHERE issue_id = :id", array("id" => $issue_id));
