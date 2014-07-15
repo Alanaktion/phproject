8aaa3bb500deb3c28418ad6dd1e924929bc77e4c
Alan Hardman <ahardman@thrivelife.com>
2014-07-15 09:04:41 -0600

    Minor updates

--- app/model/custom.php
+++ app/model/custom.php
@@ -6,6 +6,10 @@ class Custom extends Base {
 
 	protected $_table_name;
 
+	/**
+	 * Creates a custom model from a specified table name
+	 * @param string $table_name
+	 */
 	public function __construct($table_name) {
 		$this->_table_name = $table_name;
 		parent::__construct();
