d2fe3ec24f73e6c5456b00f7198b5a843359f16a
rightbit <ryan@trailjack.com>
2014-11-11 13:21:27 -0700

    extraneous $this->

--- app/model/issue/watcher.php
+++ app/model/issue/watcher.php
@@ -8,7 +8,7 @@ class Watcher extends \Model {
 
 	public function findby_watcher ($f3, $user_id = 0, $orderby = 'id') {
 		$db = $f3->get("db.instance");
-		return $this->db->exec(
+		return $db->exec(
 			'SELECT i.* FROM issue_detail i JOIN issue_watcher w on i.id = w.issue_id  '.
 			'WHERE w.user_id = :user_id AND  i.deleted_date IS NULL AND i.closed_date IS NULL AND i.status_closed = 0 '.
 			'ORDER BY :orderby ',
