7518dd3299506feb6e1868f9ed075be643a327c6
Alan Hardman <alanaktion@gmail.com>
2015-01-26 23:09:33 -0700

    Use plugin slugs in /admin/@id.

--- index.php
+++ index.php
@@ -71,23 +71,26 @@ $f3->route("GET /minify/@type/@files", function(Base $f3, $args) {
 }, $f3->get("cache_expire.minify"));
 
 // Initialize plugins and any included locales
-$plugins = scandir("app/plugin");
+$pluginDir = scandir("app/plugin");
+$plugins = array();
 $locales = "";
-foreach($plugins as $i=>$plugin) {
-	if(is_dir("app/plugin/$plugin/dict/")) {
-		$locales .= ";app/plugin/$plugin/dict/";
+foreach($pluginDir as $i=>$pluginName) {
+	if(is_dir("app/plugin/$pluginName/dict/")) {
+		$locales .= ";app/plugin/$pluginName/dict/";
 	}
 }
 if($locales) {
 	$f3->set("LOCALES", $f3->get("LOCALES") . $locales);
 }
-foreach($plugins as $i=>&$plugin) {
-	if($plugin != "." && $plugin != ".." && is_file("app/plugin/$plugin/base.php")) {
-		if(is_dir("app/plugin/$plugin/dict/")) {
-			$locales .= ";app/plugin/$plugin/dict/";
+foreach($pluginDir as $i=>$pluginName) {
+	if($pluginName != "." && $pluginName != ".." && is_file("app/plugin/$pluginName/base.php")) {
+		if(is_dir("app/plugin/$pluginName/dict/")) {
+			$locales .= ";app/plugin/$pluginName/dict/";
 		}
-		$plugin = "Plugin\\" . str_replace(" ", "_", ucwords(str_replace("_", " ", $plugin))) . "\\Base";
-		$plugin = $plugin::instance();
+		$pluginName = "Plugin\\" . str_replace(" ", "_", ucwords(str_replace("_", " ", $pluginName))) . "\\Base";
+		$plugin = $pluginName::instance();
+		$slug = \Web::instance()->slug($plugin->_package());
+		$plugins[$slug] = $plugin;
 		if(!$plugin->_installed()) {
 			try {
 				$plugin->_install();
@@ -100,8 +103,6 @@ foreach($plugins as $i=>&$plugin) {
 		} catch (Exception $e) {
 			$f3->set("error", "Failed to initialize plugin " . $plugin->_package() . ": " . $e->getMessage());
 		}
-	} else {
-		unset($plugins[$i]);
 	}
 }
 $f3->set("plugins", $plugins);
