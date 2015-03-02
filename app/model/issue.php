31c8bb768870f612aaf0a3dff861f0ebbcc0c347
Alan Hardman <ahardman@thrivelife.com>
2015-03-02 14:05:15 -0700

    Don't notify on multi-level duplication, don't duplicate deleted issues

--- app/model/issue.php
+++ app/model/issue.php
@@ -294,34 +294,31 @@ class Issue extends \Model {
 	 * @return Issue $this
 	 */
 	protected function _duplicateTree($id, $new_id) {
-
 		// Find all child issues
 		$children = $this->find(array("parent_id = ?", $id));
 		if(count($children)) {
 			$f3 = \Base::instance();
 			foreach($children as $child) {
+				if(!$child->deleted_date) {
+					// Duplicate issue
+					$child->copyto("duplicating_issue");
+					$f3->clear("duplicating_issue.id");
+					$f3->clear("duplicating_issue.due_date");
 
-				// Duplicate issue
-				$child->copyto("duplicating_issue");
-				$f3->clear("duplicating_issue.id");
-				$f3->clear("duplicating_issue.due_date");
+					$new_child = new Issue;
+					$new_child->copyfrom("duplicating_issue");
+					$new_child->clear("id");
+					$new_child->clear("due_date");
+					$new_child->author_id = $f3->get("user.id");
+					$new_child->set("parent_id", $new_id);
+					$new_child->save(false);
 
-				$new_child = new Issue;
-				$new_child->copyfrom("duplicating_issue");
-				$new_child->clear("id");
-				$new_child->clear("due_date");
-				$new_child->author_id = $f3->get("user.id");
-				$new_child->set("parent_id", $new_id);
-				$new_child->save();
-
-				// Duplicate issue's children
-				$this->_duplicateTree($child->id, $new_child->id);
-
+					// Duplicate issue's children
+					$this->_duplicateTree($child->id, $new_child->id);
+				}
 			}
 		}
-
 		return $this;
-
 	}
 
 	/**
