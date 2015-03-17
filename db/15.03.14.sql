47765ce27e2187371c753aa7075707fc73f701b5
Alan Hardman <alanaktion@gmail.com>
2015-03-17 09:22:17 -0600

    Better issue_update update, not required, but better.

--- db/15.03.14.sql
+++ db/15.03.14.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `issue_update` ADD COLUMN `notify` TINYINT(1) UNSIGNED DEFAULT 0  NOT NULL AFTER `comment_id`;
+ALTER TABLE `issue_update` ADD COLUMN `notify` TINYINT(1) UNSIGNED NULL AFTER `comment_id`;
 UPDATE `config` SET `value` = '15.03.14' WHERE `attribute` = 'version';
