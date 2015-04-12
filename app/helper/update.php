dd8a23f0f783fe26b0f8c607584f4af5ca0829a2
Alan Hardman <alanaktion@gmail.com>
2015-04-12 11:39:08 -0600

    Adding an object cache for issue update human readable display

--- app/helper/update.php
+++ app/helper/update.php
@@ -4,6 +4,8 @@ namespace Helper;
 
 class Update extends \Prefab {
 
+	protected $cache = array();
+
 	/**
 	 * Generate human-readable data for issue updates
 	 * @param  string $field
@@ -12,100 +14,154 @@ class Update extends \Prefab {
 	 * @return array
 	 */
 	function humanReadableValues($field, $old_val, $new_val) {
-		switch($field) {
-			case "owner_id":
-			case "author_id":
-				// Load old and new users and get names
-				$user = new \Model\User;
-				if(is_numeric($old_val)) {
-					$user->load($old_val);
-					$old_val = $user->name;
-				}
-				if(is_numeric($new_val)) {
-					$user->load($new_val);
-					$new_val = $user->name;
-				}
-				break;
-			case "status":
-				// Load old and new statuses and get names
-				$status = new \Model\Issue\Status;
-				if(is_numeric($old_val)) {
-					$status->load($old_val);
-					$old_val = $status->name;
-				}
-				if(is_numeric($new_val)) {
-					$status->load($new_val);
-					$new_val = $status->name;
-				}
-				break;
-			case "priority":
-				// Load old and new priorities and get names
-				$priority = new \Model\Issue\Priority;
-				if(is_numeric($old_val)) {
-					$priority->load(array("value = ?", $old_val));
-					$old_val = $priority->name;
-				}
-				if(is_numeric($new_val)) {
-					$priority->load(array("value = ?", $new_val));
-					$new_val = $priority->name;
-				}
-				break;
-			case "parent_id":
-				// Load old and new parent issues and get names
-				$name = "Parent";
-				$issue = new \Model\Issue;
-				if(is_numeric($old_val)) {
-					$issue->load($old_val);
-					$old_val = $issue->name;
-				}
-				if(is_numeric($new_val)) {
-					$issue->load($new_val);
-					$new_val = $issue->name;
-				}
-				break;
-			case "sprint_id":
-				// Load old and new sprints and get names and dates
-				$name = "Sprint";
-				$sprint = new \Model\Sprint;
-				if(is_numeric($old_val)) {
-					$sprint->load($old_val);
-					$old_val = $sprint->name . " - " . date('n/j', strtotime($sprint->start_date)) . "-" . date('n/j', strtotime($sprint->end_date));
-				}
-				if(is_numeric($new_val)) {
-					$sprint->load($new_val);
-					$new_val = $sprint->name . " - " . date('n/j', strtotime($sprint->start_date)) . "-" . date('n/j', strtotime($sprint->end_date));
-				}
-				break;
-			case "type_id":
-				// Load old and new issue types and get names
-				$name = "Type";
-				$type = new \Model\Issue\Type;
-				if(is_numeric($old_val)) {
-					$type->load($old_val);
-					$old_val = $type->name;
-				}
-				if(is_numeric($new_val)) {
-					$type->load($new_val);
-					$new_val = $type->name;
-				}
-				break;
-			case "hours_total":
-				$name = "Planned Hours";
-				break;
-			case "hours_remaining":
-				$name = "Remaining Hours";
-				break;
-			case "hours_spent":
-				$name = "Spent Hours";
-				break;
+		$f3 = \Base::instance();
+
+		// Generate human readable values
+		$func = "convert" . ucfirst($f3->camelcase($field));
+		if(is_callable(array($this, $func))) {
+			if($old_val) {
+				$old_val = call_user_func_array(
+						array($this, $func), array($old_val));
+			}
+			if($new_val) {
+				$new_val = call_user_func_array(
+						array($this, $func), array($new_val));
+			}
 		}
 
-		// Generate human readable field name if not already specified
+		// Generate human readable field name
+		$name = $f3->get("dict.cols." . $field);
 		if(empty($name)) {
-			$name = ucwords(str_replace(array("_", " id"), array(" ", ""), $field));
+			$name = ucwords(str_replace(
+					array("_", " id"), array(" ", ""), $field));
 		}
 
 		return array("field" => $name, "old" => $old_val, "new" => $new_val);
+	}
+
+	/**
+	 * Convert a user ID to a user name
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertUserId($id) {
+ 		if(isset($this->cache['user.' . $id])) {
+			$user = $this->cache['user.' . $id];
+		} else {
+			$user = new \Model\User;
+			$user->load($id);
+			$this->cache['user.' . $id] = $user;
+		}
+		return $user->name;
+	}
+
+	/**
+	 * Convert an owner user ID to a name
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertOwnerId($id) {
+		return convertUserId($id);
+	}
+
+	/**
+	 * Convert an author user ID to a name
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertAuthorId($id) {
+		return convertUserId($id);
+	}
+
+	/**
+	 * Convert a status ID to a name
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertStatus($id) {
+ 		if(isset($this->cache['status.' . $id])) {
+			$status = $this->cache['status.' . $id];
+		} else {
+			$status = new \Model\Issue\Status;
+			$status->load($id);
+			$this->cache['status.' . $id] = $status;
+		}
+		return $status->name;
+	}
+
+	/**
+	 * Convert a priority ID to a name
+	 * @param int $value
+	 * @return string
+	 */
+	public function convertPriority($value) {
+ 		if(isset($this->cache['priority.' . $value])) {
+			$priority = $this->cache['priority.' . $value];
+		} else {
+			$priority = new \Model\Issue\Priority;
+			$priority->load(array("value = ?", $value));
+			$this->cache['priority.' . $value] = $priority;
+		}
+		return $priority->name;
+	}
+
+	/**
+	 * Convert an issue ID to a name
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertIssueId($id) {
+ 		if(isset($this->cache['issue.' . $id])) {
+			$issue = $this->cache['issue.' . $id];
+		} else {
+			$issue = new \Model\Issue;
+			$issue->load($id);
+			$this->cache['issue.' . $id] = $issue;
+		}
+		return $issue->name;
+	}
+
+	/**
+	 * Convert a parent issue ID to a name
+	 * @param int $id
+	 * @param string
+	 */
+	public function convertParentId($id) {
+		return $this->convertIssueId($id);
+	}
+
+	/**
+	 * Convert a sprint ID to a name/date
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertSprintId($id) {
+ 		if(isset($this->cache['sprint.' . $id])) {
+			$sprint = $this->cache['sprint.' . $id];
+		} else {
+			$sprint = new \Model\Sprint;
+			$sprint->load($id);
+			$this->cache['sprint.' . $id] = $sprint;
+		}
+		return $sprint->name . " - " .
+				date('n/j', strtotime($sprint->start_date)) . "-" .
+				date('n/j', strtotime($sprint->end_date));
+	}
+
+	/**
+	 * Convert a sprint ID to a name/date
+	 * @param int $id
+	 * @return string
+	 */
+	public function convertTypeId($id) {
+ 		if(isset($this->cache['type.' . $id])) {
+			$type = $this->cache['type.' . $id];
+		} else {
+			$type = new \Model\Issue\Type;
+			$type->load($id);
+			$this->cache['type.' . $id] = $type;
+		}
+		return $type->name;
 	}
 
 }
