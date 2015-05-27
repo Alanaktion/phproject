69cf5cc0ba5c1dec5b65714d5c7a953a447256f9
rightbit <ryan@trailjack.com>
2015-05-27 17:58:03 -0600

    Find groups by user

--- app/model/user/group.php
+++ app/model/user/group.php
@@ -6,5 +6,27 @@ class Group extends \Model {
 
 	protected $_table_name = "user_group";
 
+	/**
+	 * Get complete group list for user
+	 * @return array
+	 */
+	public function getUserGroups($user_id = 0) {
+		$f3 = \Base::instance();
+		$db = $f3->get("db.instance");
+
+		if(empty($user_id)) {
+			$user_id =  $f3->get("user.id");
+		}
+
+		$query_groups = "SELECT u.id, u.name, u.username
+			FROM user u
+			JOIN user_group g ON u.id = g.group_id
+			WHERE g.user_id = :user AND u.deleted_date IS NULL ORDER BY u.name";
+
+		$result = $db->exec($query_groups, array(":user" => $user_id));
+		return $result;
+
+	}
+
 }
 
