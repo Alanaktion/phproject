0811fa8121d8221a8481270f3e6529db775e7734
Alan Hardman <ahardman@thrivelife.com>
2015-07-17 17:27:39 -0600

    Minor additions to Config and Issue\File

--- app/model/issue/file.php
+++ app/model/issue/file.php
@@ -5,6 +5,21 @@ namespace Model\Issue;
 class File extends \Model {
 
 	protected $_table_name = "issue_file";
+	protected static $requiredFields = array("issue_id", "user_id", "filename", "disk_filename");
+
+	/**
+	 * Create and save a new file, optionally sending notifications
+	 * @param  array $data
+	 * @param  bool  $notify
+	 * @return Comment
+	 */
+	public static function create(array $data, $notify = true) {
+		$item = parent::create($data);
+		if($notify) {
+			$notification = \Helper\Notification::instance();
+			$notification->issue_file($item->issue_id, $item->id);
+		}
+		return $item;
+	}
 
 }
-
