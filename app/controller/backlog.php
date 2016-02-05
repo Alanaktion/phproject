9734dcb8cb5b837fe44281c81e1fbf48c364d568
Alan Hardman <ahardman@thrivelife.com>
2016-02-05 10:22:04 -0700

    Fixing empty array issue with backlog

--- app/controller/backlog.php
+++ app/controller/backlog.php
@@ -96,6 +96,7 @@ class Backlog extends \Controller {
 			$backlog = array();
 			$sortModel = new \Model\Issue\Backlog;
 			$sortModel->load(array("user_id = ?", $params["groupid"]));
+			$sortArray = array();
 			if($sortModel->id) {
 				$sortArray = json_decode($sortModel->issues);
 				foreach($sortArray as $id) {
