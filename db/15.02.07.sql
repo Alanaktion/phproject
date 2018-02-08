2b14f6280df519643821312db18e2bae4e53aaba
Alan Hardman <alanaktion@gmail.com>
2018-02-08 00:09:27 -0700

    Fixing upgrade scripts on MySQL 8.0
    This likely fixes #328

--- db/15.02.07.sql
+++ db/15.02.07.sql
@@ -7,7 +7,7 @@ Rank 4: admin - current admin privileges, minus plugin config
 Rank 5: superadmin - able to change config file values from web interface
 */
 
-ALTER TABLE user ADD COLUMN rank tinyint(1) UNSIGNED DEFAULT 0 NOT NULL AFTER role;
-UPDATE user SET rank = '2' WHERE role = 'user';
-UPDATE user SET rank = '4' WHERE role = 'admin';
+ALTER TABLE user ADD COLUMN rank tinyint(1) UNSIGNED DEFAULT 0 NOT NULL AFTER `role`;
+UPDATE user SET rank = '2' WHERE `role` = 'user';
+UPDATE user SET rank = '4' WHERE `role` = 'admin';
 UPDATE config SET value = '15.02.07' WHERE attribute = 'version';
--- db/16.11.23.sql
+++ db/16.11.23.sql
@@ -5,10 +5,10 @@ ALTER TABLE `issue_type`
 
 UPDATE issue_type
 JOIN config ON config.value = issue_type.id AND config.attribute = 'issue_type.project'
-SET issue_type.role = 'project';
+SET issue_type.`role` = 'project';
 
 UPDATE issue_type
 JOIN config ON config.value = issue_type.id AND config.attribute = 'issue_type.bug'
-SET issue_type.role = 'bug';
+SET issue_type.`role` = 'bug';
 
 UPDATE `config` SET `value` = '16.11.23' WHERE `attribute` = 'version';
