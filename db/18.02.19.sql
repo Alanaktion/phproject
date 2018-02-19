d29b2f6b9af77257658acc1f749cddb71e923e73
Alan Hardman <alanaktion@gmail.com>
2018-02-19 11:01:46 -0700

    Fixing an issue with upgrades to 1.7.2
    This will be a bugfix release 1.7.3. Thanks to @jseutens for catching this in #345!

---
+++ db/18.02.19.sql
@@ -0,0 +1,2 @@
+UPDATE `config` SET `attribute` = 'session_lifetime' WHERE `attribute` = 'JAR.lifetime';
+UPDATE `config` SET `value` = '18.02.19' WHERE `attribute` = 'version';
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('PHPROJECT_VERSION', '1.7.2');
+define('PHPROJECT_VERSION', '1.7.3');
 
 // Initialize core
 require_once "vendor/autoload.php";
