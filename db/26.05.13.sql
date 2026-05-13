78fb58d0547ce667f0fc4c0839b8915194725809
Alan <alanaktion@gmail.com>
2026-05-13 16:54:58 -0600

    Modify password column expansion for MySQL
    Updated SQL syntax to only be run on MySQL.

--- db/26.05.13.sql
+++ db/26.05.13.sql
@@ -1,6 +1,6 @@
-# Expand password column for modern password hashes
-ALTER TABLE `user`
-	MODIFY COLUMN `password` VARCHAR(80) NULL;
+# Expand password column for modern password hashes (MySQL only)
+/*!50000 ALTER TABLE `user`
+	MODIFY COLUMN `password` VARCHAR(80) NULL; */
 
 # Update version
 UPDATE `config` SET `value` = '26.05.13' WHERE `attribute` = 'version';
